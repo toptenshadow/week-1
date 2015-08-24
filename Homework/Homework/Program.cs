00cc926164dd3a7b4f39a611f85205c247963606
toptenshadow <topten_non-prince@hotmail.com>
2015-08-24 16:52:30 +0700

    input number

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -22,7 +22,9 @@ namespace HW1
         }
         static void Main(string[] args)
         {
-            int j = 5;
+            int j;
+            Console.WriteLine("Input your number : ");
+            j = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(process(j));
             Console.ReadKey();
         }
