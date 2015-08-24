02d912a78ae41a8c80df01219777c9e385b1e114
toptenshadow <topten_non-prince@hotmail.com>
2015-08-24 15:39:13 +0700

    without flag

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -10,15 +10,15 @@ namespace Lab1_1
     {
         static void Main(string[] args)
         {
-            bool flag = true;
+            //bool flag = true;
 
             // input
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
 
             // process
-            while (flag)
+           // while (flag)
             {
-                flag = false;
+               // flag = false;
                 for (int i = 0; i != input.Length - 1; i++)
                 {
                     if (input[i] < input[i + 1])
@@ -26,7 +26,7 @@ namespace Lab1_1
                         int temp = input[i];
                         input[i] = input[i + 1];
                         input[i + 1] = temp;
-                        flag = true;
+                       // flag = true;
                     }
                 }
             }
